b11368b9c4cd30be9a7bae1e0da0cbe90bea1830
Tarun <tbhuria@gmail.com>
2020-10-19 21:12:42 +0530

    Login page created

---
+++ src/common/header/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./Header.css";
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="app-header">
+          <div className="app-logo">
+        Image Viewer
+      </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
